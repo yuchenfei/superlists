6dd41174d275e0cf1a00bdd608dd144f47065461
yuchenfei <954833373@qq.com>
2017-01-22 00:42:43 +0800

    功能测试：+检查是否可以输入一个待办事项

--- functional_tests.py
+++ functional_tests.py
@@ -6,6 +6,7 @@
 # @Software: PyCharm
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -23,17 +24,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # 网页的标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
 
         # 应用邀请输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # 在文本框输入“Buy peacock feathers”
+        inputbox.send_keys('Buy peacock feathers')
 
         # 按下回车页面更新
         # 待办事项表格中显示了“1:Buy peacock feathers”
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1:Buy peacock feathers' for row in rows)
+        )
 
         # 页面中又显示了一个文本框，可以输入其他待办事项
         # 输入“Use peacock feathers to make a fly”
+        self.fail('Finish the test!')
 
         # 页面再次更新，清单中显示了这两个待办事项
 
