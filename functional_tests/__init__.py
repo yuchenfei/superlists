5f8c5a18be4eb35542f4f69db2249eb7abf89768
yuchenfei <954833373@qq.com>
2017-01-22 22:14:22 +0800

    创建功能测试模块，使用LiveServerTestCase

---
+++ functional_tests/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/1/22 22:00
+# @Author  : yuchenfei
+# @File    : __init__.py
+# @Software: PyCharm
--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2017/1/21 0:22
 # @Author  : yuchenfei
-# @File    : functional_tests.py
+# @File    : tests.py
 # @Software: PyCharm
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
@@ -25,7 +25,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrive_it_later(self):
         # 打开应用首页
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # 网页的标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
@@ -65,7 +65,3 @@ class NewVisitorTest(unittest.TestCase):
         # 访问了这个URL，发现待办事项列表还在
 
         # 离开
-
-
-if __name__ == '__main__':
-    unittest.main()
