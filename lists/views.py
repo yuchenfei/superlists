f813123c327799ece8795a4ef8aae0d2ff373c14
yuchenfei <954833373@qq.com>
2017-01-23 14:14:25 +0800

    调整模型，待办事项和清单关联起来

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 
 
-def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
-    return redirect('/lists/the-only-list-in-the-world/')
+def home_page(request):
+    return render(request, 'home.html')
 
 
 def view_list(request):
@@ -12,5 +11,7 @@ def view_list(request):
     return render(request, 'list.html', {'items': items})
 
 
-def home_page(request):
-    return render(request, 'home.html')
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'],list=list_)
+    return redirect('/lists/the-only-list-in-the-world/')
