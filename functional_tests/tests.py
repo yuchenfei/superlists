3476c4cd397dbc052fc73ef294f7963d54b517f3
yuchenfei <954833373@qq.com>
2017-01-23 22:22:27 +0800

    功能测试：+布局和样式测试

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -89,3 +89,25 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # 离开
+
+    def test_layout_and_styling(self):
+        # 访问首页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # 看到输入框完美居中显示
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # 新建了一个清单，看到输入框仍完美居中显示
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
