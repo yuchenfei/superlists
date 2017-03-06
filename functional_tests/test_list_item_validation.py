632c583661c88a42d660a3708e16903ead192c7a
yuchenfei <954833373@qq.com>
2017-03-06 21:23:17 +0800

    功能测试：+避免提交空待办事项

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -12,15 +12,27 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # 用户访问了首页，不小行提交了一个空待办事项
         # 输入框中没有输入内容，他就按下了回车键
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # 首页刷新了，显示了一个错误消息
         # 提示待办事项不能为空
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # 他输入了一些文字，然后再次提交，这次没问题了
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1:Buy milk')
 
         # 他又提交了一个空待办事项
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # 在清单页面他看到了一个类似的错误消息
+        self.check_for_row_in_list_table('1:Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # 输入文字之后就没问题了
-        self.fail('write me!')
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1:Buy milk')
+        self.check_for_row_in_list_table('2:Make tea')
