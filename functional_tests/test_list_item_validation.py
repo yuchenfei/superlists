aa4c2ca07f4cfd4115c20cf204ae824bd0041463
yuchenfei <954833373@qq.com>
2017-03-07 22:55:20 +0800

    调整列表视图来验证模型是否有效

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -22,17 +22,17 @@ class ItemValidationTest(FunctionalTest):
 
         # 他输入了一些文字，然后再次提交，这次没问题了
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
-        self.check_for_row_in_list_table('1:Buy milk')
+        self.check_for_row_in_list_table('1: Buy milk')
 
         # 他又提交了一个空待办事项
         self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # 在清单页面他看到了一个类似的错误消息
-        self.check_for_row_in_list_table('1:Buy milk')
+        self.check_for_row_in_list_table('1: Buy milk')
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # 输入文字之后就没问题了
         self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
-        self.check_for_row_in_list_table('1:Buy milk')
-        self.check_for_row_in_list_table('2:Make tea')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
