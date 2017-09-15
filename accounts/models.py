9c810855eb13b643db10fc16ad04ae6996db681d
yuchenfei <yuchenfei19950319@gmail.com>
2017-09-15 16:37:43 +0800

    实现用户邮箱认证模型

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    is_anonymous = False
+    is_authenticated = True
+
+
+class Token(models.Model):
+    email = models.EmailField()
+    uid = models.CharField(default=uuid.uuid4, max_length=40)
---
+++ accounts/tests/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/9/14 16:31
+# @Author  : YuChenFei
+# @File    : __init__.py.py
+# @Software: PyCharm
