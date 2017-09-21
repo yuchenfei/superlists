fa5fef5e6d4606c11b1b172247c9bc1dfa756ed2
yuchenfei <yuchenfei19950319@gmail.com>
2017-09-21 08:58:32 +0800

    修复部署时发现的一个语法问题

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,7 @@ def send_login_email(request):
     url = request.build_absolute_uri(
         reverse('login') + '?token=' + str(token.uid)
     )
-    message_body = f'Use this link to log in:\n\n{url}'
+    message_body = 'Use this link to log in:\n\n{url}'.format(url=url)
     send_mail(
         'Your login link for Superlists',
         message_body,
