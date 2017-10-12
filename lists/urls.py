8a222d89ae6256e1f5135bd94518d722b8b42db1
yuchenfei <yuchenfei19950319@gmail.com>
2017-10-12 15:08:16 +0800

    修改网页模板层级结构 添加单元测试：个人清单使用对应的模板 添加对应URL解析和试图函数

--- lists/urls.py
+++ lists/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from lists import views
 
 urlpatterns = [
-    url(r'^(\d+)/$', views.view_list, name='view_list'),
     url(r'^new$', views.new_list, name='new_list'),
+    url(r'^(\d+)/$', views.view_list, name='view_list'),
+    url(r'^users/(.+)/$', views.my_lists, name='my_lists'),
 ]
