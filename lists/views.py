7bf1cdb49bc56d01d659131bf7e7d7d4d3837564
yuchenfei <yuchenfei19950319@gmail.com>
2017-10-15 17:50:20 +0800

    new_list视图尝试分配属主，但不行

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,11 @@
 from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
 
 from lists.forms import ExistingListItemForm, ItemForm
 from lists.models import Item, List
+
+User = get_user_model()
 
 
 def home_page(request):
@@ -23,7 +26,9 @@ def view_list(request, list_id):
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
-        list_ = List.objects.create()
+        list_ = List()
+        list_.owner = request.user
+        list_.save()
         form.save(for_list=list_)
         return redirect(list_)
     else:
@@ -31,4 +36,5 @@ def new_list(request):
 
 
 def my_lists(request, email):
-    return render(request, 'my_lists.html')
+    owner = User.objects.get(email=email)
+    return render(request, 'my_lists.html', {'owner': owner})
