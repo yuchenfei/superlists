2dd06a8e28b7cc904367cbebdaa2db0ec7a618ea
yuchenfei <yuchenfei19950319@gmail.com>
2017-10-16 22:03:38 +0800

    用户清单功能完成

--- lists/models.py
+++ lists/models.py
@@ -9,6 +9,10 @@ class List(models.Model):
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
 
+    @property
+    def name(self):
+        return self.item_set.first().text
+
 
 class Item(models.Model):
     text = models.TextField(default='')
@@ -17,3 +21,6 @@ class Item(models.Model):
     class Meta:
         ordering = ('id',)
         unique_together = ('list', 'text')
+
+    def __str__(self):
+        return self.text
